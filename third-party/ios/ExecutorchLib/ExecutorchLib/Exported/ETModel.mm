e396d45f3ad5866a959bd72675184eea44c08009
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2024-12-04 15:27:25 +0100

    feat: added methods to extract input type and shape from model(ios)

--- third-party/ios/ExecutorchLib/ExecutorchLib/Exported/ETModel.mm
+++ third-party/ios/ExecutorchLib/ExecutorchLib/Exported/ETModel.mm
@@ -29,6 +29,45 @@ using namespace ::torch::executor;
   return @((int)err);
 }
 
+- (NSArray *)getInputShape {
+  const auto method_meta = _model->method_meta("forward");
+  
+  if(method_meta.ok()){
+    const auto input_meta = method_meta->input_tensor_meta(0);
+    if(input_meta.ok()){
+      const auto shape = input_meta->sizes();
+      NSMutableArray *nsShape = [[NSMutableArray alloc] init];
+      
+      for(int i = 0; i < shape.size(); i++) {
+        [nsShape addObject:@(shape[i])];
+      }
+      
+      return [nsShape copy];
+    }
+  }
+  
+  return nil;
+};
+
+- (NSNumber *)getInputType {
+  const auto method_meta = _model->method_meta("forward");
+  
+  if(method_meta.ok()){
+    const auto input_meta = method_meta->input_tensor_meta(0);
+    if(input_meta.ok()){
+      switch(input_meta->scalar_type()) {
+        case ScalarType::Byte: return @0;
+        case ScalarType::Int: return @1;
+        case ScalarType::Long: return @2;
+        case ScalarType::Float: return @3;
+        case ScalarType::Double: return @4;
+      }
+    }
+  }
+  
+  return @-1;
+};
+
 - (NSArray *)forward:(NSArray *)input
                shape:(NSArray *)shape
            inputType:(NSNumber *)inputType {
@@ -37,49 +76,50 @@ using namespace ::torch::executor;
   ssize_t outputNumel = 0;
   @try {
     switch (inputTypeIntValue) {
-    case 0: {
-      // Int8Array
-      const int8_t *output =
-          runForwardFromNSArray<int8_t>(input, outputNumel, shapes, _model);
-      return arrayToNSArray<int8_t>(output, outputNumel);
-    }
-    case 1: {
-      // Int32Array
-      const int32_t *output =
-          runForwardFromNSArray<int32_t>(input, outputNumel, shapes, _model);
-      return arrayToNSArray<int32_t>(output, outputNumel);
-    }
-    case 2: {
-      // BigInt64Array
-      const int64_t *output =
-          runForwardFromNSArray<int64_t>(input, outputNumel, shapes, _model);
-      return arrayToNSArray<int64_t>(output, outputNumel);
-    }
-    case 3: {
-      // Float32Array
-      const float *output =
-          runForwardFromNSArray<float>(input, outputNumel, shapes, _model);
-      return arrayToNSArray<float>(output, outputNumel);
-    }
-    case 4: {
-      // Float64Array
-      const double *output =
-          runForwardFromNSArray<double>(input, outputNumel, shapes, _model);
-      return arrayToNSArray<double>(output, outputNumel);
-    }
+      case 0: {
+        // Int8Array
+        const int8_t *output =
+        runForwardFromNSArray<int8_t>(input, outputNumel, shapes, _model);
+        return arrayToNSArray<int8_t>(output, outputNumel);
+      }
+      case 1: {
+        // Int32Array
+        const int32_t *output =
+        runForwardFromNSArray<int32_t>(input, outputNumel, shapes, _model);
+        return arrayToNSArray<int32_t>(output, outputNumel);
+      }
+      case 2: {
+        // BigInt64Array
+        const int64_t *output =
+        runForwardFromNSArray<int64_t>(input, outputNumel, shapes, _model);
+        return arrayToNSArray<int64_t>(output, outputNumel);
+      }
+      case 3: {
+        // Float32Array
+        const float *output =
+        runForwardFromNSArray<float>(input, outputNumel, shapes, _model);
+        return arrayToNSArray<float>(output, outputNumel);
+      }
+      case 4: {
+        // Float64Array
+        const double *output =
+        runForwardFromNSArray<double>(input, outputNumel, shapes, _model);
+        return arrayToNSArray<double>(output, outputNumel);
+      }
     }
   } @catch (NSException *exception) {
     NSInteger originalCode = [exception.reason integerValue];
     @throw [NSException
-        exceptionWithName:@"forward_error"
-                   reason:[NSString stringWithFormat:@"%ld", (long)originalCode]
-                 userInfo:nil];
+            exceptionWithName:@"forward_error"
+            reason:[NSString stringWithFormat:@"%ld", (long)originalCode]
+            userInfo:nil];
   }
   // throwing an RN-ET exception
   @
   throw [NSException exceptionWithName:@"forward_error"
                                 reason:[NSString stringWithFormat:@"%d",
-                                                                  0x65] // 101
+                                        0x65] // 101
                               userInfo:nil];
 }
+
 @end
