dfc95e80d7069015ff80d334173eae31d6c1a5a2
jakmro <jakub.mroz@swmansion.com>
2024-12-19 12:08:06 +0100

    Remove spaces

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -2,6 +2,6 @@
 import MDXComponents from '@theme-original/MDXComponents';
 
 export default {
-    // Re-use the default mapping
-    ...MDXComponents,
+  // Re-use the default mapping
+  ...MDXComponents,
 };
