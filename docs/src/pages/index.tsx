c18330d884a098c68bcff1b740ccd6a43b2f4dcf
jakmro <jakub.mroz@swmansion.com>
2024-12-28 01:20:47 +0100

    Add features list

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Redirect } from '@docusaurus/router';
+import Layout from '@theme/Layout';
+import styles from './styles.module.css';
+import HomepageStartScreen from '@site/src/components/Hero/StartScreen';
+import ReactNativeExecuTorchFeatures from '@site/src/components/ReactNativeExecuTorchFeatures';
+import { HireUsSection } from '@swmansion/t-rex-ui';
 
 function Home() {
   return (
-    <Redirect to="/react-native-executorch/docs/fundamentals/getting-started" />
+    <Layout
+      title={`React Native React Native ExecuTorch`}
+      description="Declarative way to run AI models in React Native on device, powered by ExecuTorch."
+    >
+      <div className={styles.container}>
+        <HomepageStartScreen />
+        <ReactNativeExecuTorchFeatures />
+        <HireUsSection
+          href={
+            'https://swmansion.com/contact/projects?utm_source=react-native-executorch&utm_medium=docs'
+          }
+        />
+      </div>
+    </Layout>
   );
 }
 
