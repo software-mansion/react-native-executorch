7885df99f9ff8151814351f124c93db5d75aea13
chmjkb <jakubchmura1607@gmail.com>
2025-01-22 09:22:18 +0100

    replace single input forward with multiple inputs

--- src/native/NativeETModule.ts
+++ src/native/NativeETModule.ts
@@ -5,11 +5,10 @@ export interface Spec extends TurboModule {
   loadModule(modelSource: string): Promise<number>;
 
   forward(
-    input: number[],
-    shape: number[],
-    inputType: number
-  ): Promise<number[]>;
-
+    inputs: number[],
+    shapes: number[],
+    inputTypes: number[]
+): Promise<number[]>;
   loadMethod(methodName: string): Promise<number>;
 }
 
