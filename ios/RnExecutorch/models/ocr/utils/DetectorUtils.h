4bcb98b617e03e688f8a589ecfa8afa37bd854f2
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-01-27 09:22:25 +0100

    fix: add missing argument in header file

--- ios/RnExecutorch/models/ocr/utils/DetectorUtils.h
+++ ios/RnExecutorch/models/ocr/utils/DetectorUtils.h
@@ -7,7 +7,7 @@
 + (NSArray *)getDetBoxes:(cv::Mat)textmap linkMap:(cv::Mat)linkmap textThreshold:(double)textThreshold linkThreshold:(double)linkThreshold lowText:(double)lowText;
 + (NSMutableArray *)prepareBoxesFromPolys:(NSArray *)polys;
 + (NSMutableArray *)combineBoxes:(NSMutableArray *)boxes withYCenterThs:(CGFloat)ycenterThs heightThs:(CGFloat)heightThs;
-+ (NSArray *)mergeBoxes:(NSMutableArray *)combinedBoxes withWidthThs:(CGFloat)widthThs addMargin:(CGFloat)addMargin;
++ (NSArray *)mergeBoxes:(NSMutableArray *)combinedBoxes withWidthThs:(CGFloat)widthThs heightThs:(CGFloat)heightThs addMargin:(CGFloat)addMargin;
 + (NSArray<NSArray<NSNumber *> *> *)groupTextBox:(NSArray<NSArray<NSNumber *> *> *)polys
                                       ycenterThs:(CGFloat)ycenterThs
                                        heightThs:(CGFloat)heightThs
