dfdc8110d035331bc268c26ac67bc57d6a62281f
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-02-05 16:21:10 +0100

    feat: implemented upgraded mid processing pipeline

--- ios/RnExecutorch/models/ocr/utils/RecognizerUtils.h
+++ ios/RnExecutorch/models/ocr/utils/RecognizerUtils.h
@@ -9,7 +9,7 @@
 + (cv::Mat)divideMatrix:(cv::Mat)matrix byVector:(NSArray<NSNumber *> *)vector;
 + (cv::Mat)softmax:(cv::Mat)inputs;
 + (NSDictionary *)calculateResizeRatioAndPaddings:(int)width height:(int)height desiredWidth:(int)desiredWidth desiredHeight:(int)desiredHeight;
-+ (cv::Mat)getCroppedImage:(int)x_max x_min:(int)x_min y_max:(int)y_max y_min:(int)y_min image:(cv::Mat)image modelHeight:(int)modelHeight;
++ (cv::Mat)getCroppedImage:(NSDictionary *)box image:(cv::Mat)image modelHeight:(int)modelHeight;
 + (NSMutableArray *)sumProbabilityRows:(cv::Mat)probabilities modelOutputHeight:(int)modelOutputHeight;
 + (NSArray *)findMaxValuesAndIndices:(cv::Mat)probabilities;
 + (double)computeConfidenceScore:(NSArray<NSNumber *> *)valuesArray indicesArray:(NSArray<NSNumber *> *)indicesArray;
