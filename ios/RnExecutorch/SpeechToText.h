b39f57bb3d87b154dcbb21779bba38589eebdefd
chmjkb <jakubchmura1607@gmail.com>
2025-02-20 14:07:58 +0100

    chore: remove unused comments

--- ios/RnExecutorch/SpeechToText.h
+++ ios/RnExecutorch/SpeechToText.h
@@ -1,7 +1,5 @@
 #import <RnExecutorchSpec/RnExecutorchSpec.h>
 
-//@interface SpeechToText : NSObject <NativeSpeechToTextSpec>
-//@interface LLM : NativeLLMSpecBase <NativeLLMSpec>
 @interface SpeechToText : NativeSpeechToTextSpecBase <NativeSpeechToTextSpec>
 
 @end
