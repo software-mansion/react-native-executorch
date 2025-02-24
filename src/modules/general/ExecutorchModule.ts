5e748b38d07c3fd742d8e6adff0dfa5c96ba37ba
chmjkb <jakubchmura1607@gmail.com>
2025-02-24 08:22:49 +0100

    feat: add multiple inputs to hookless api

--- src/modules/general/ExecutorchModule.ts
+++ src/modules/general/ExecutorchModule.ts
@@ -1,20 +1,35 @@
 import { BaseModule } from '../BaseModule';
 import { ETError, getError } from '../../Error';
 import { _ETModule } from '../../native/RnExecutorchModules';
-import { ETInput, getTypeIdentifier } from '../../types/common';
+import { ETInput } from '../../types/common';
+import { getTypeIdentifier } from '../../useModule';
 
 export class ExecutorchModule extends BaseModule {
   static module = new _ETModule();
 
-  static async forward(input: ETInput, shape: number[]) {
-    const inputType = getTypeIdentifier(input);
-    if (inputType === -1) {
-      throw new Error(getError(ETError.InvalidArgument));
+  static async forward(input: ETInput[] | ETInput, shape: number[][]) {
+    if (!Array.isArray(input)) {
+      input = [input];
+    }
+
+    let inputTypeIdentifiers = [];
+    let modelInputs = [];
+
+    for (let idx = 0; idx < input.length; idx++) {
+      let currentInputTypeIdentifier = getTypeIdentifier(input[idx] as ETInput);
+      if (currentInputTypeIdentifier === -1) {
+        throw new Error(getError(ETError.InvalidArgument));
+      }
+      inputTypeIdentifiers.push(currentInputTypeIdentifier);
+      modelInputs.push([...(input[idx] as unknown as number[])]);
     }
 
     try {
-      const numberArray = [...input] as number[];
-      return await this.module.forward(numberArray, shape, inputType);
+      return await this.module.forward(
+        modelInputs,
+        shape,
+        inputTypeIdentifiers
+      );
     } catch (e) {
       throw new Error(getError(e));
     }
