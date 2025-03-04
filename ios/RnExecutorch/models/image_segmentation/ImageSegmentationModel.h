cda498682e415560c3a52d001108b12275b4c75f
Jakub Gonera <jakub.gonera@swmansion.com>
2025-03-04 11:12:55 +0100

    Add working ios-native model

--- ios/RnExecutorch/models/image_segmentation/ImageSegmentationModel.h
+++ ios/RnExecutorch/models/image_segmentation/ImageSegmentationModel.h
@@ -1,5 +1,8 @@
 #import "../BaseModel.h"
+#import "opencv2/opencv.hpp"
 
 @interface ImageSegmentationModel : BaseModel
+- (cv::Size)getModelImageSize;
+- (NSDictionary *)runModel:(cv::Mat &)input;
 
 @end
