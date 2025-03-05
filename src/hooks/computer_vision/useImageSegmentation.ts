3d04da27351086de39ff520be91181794222eaf6
Jakub Gonera <jakub.gonera@swmansion.com>
2025-03-05 12:14:27 +0100

    Add a way to filter what segmentation classes are returned

--- src/hooks/computer_vision/useImageSegmentation.ts
+++ src/hooks/computer_vision/useImageSegmentation.ts
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { _ImageSegmentationModule } from '../../native/RnExecutorchModules';
-import { useModule } from '../useModule';
+import { fetchResource } from '../../utils/fetchResource';
+import { ETError, getError } from '../../Error';
 
 interface Props {
   modelSource: string | number;
@@ -13,19 +14,52 @@ export const useImageSegmentation = ({
   isReady: boolean;
   isGenerating: boolean;
   downloadProgress: number;
-  forward: (input: string) => Promise<{ [category: string]: number[] }>;
+  forward: (
+    input: string,
+    classesOfInterest?: string[]
+  ) => Promise<{ [category: string]: number[] }>;
 } => {
   const [module, _] = useState(() => new _ImageSegmentationModule());
-  const {
-    error,
-    isReady,
-    isGenerating,
-    downloadProgress,
-    forwardImage: forward,
-  } = useModule({
-    modelSource,
-    module,
-  });
+  const [error, setError] = useState<null | string>(null);
+  const [isReady, setIsReady] = useState(false);
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      if (!modelSource) return;
+
+      try {
+        setIsReady(false);
+        const fileUri = await fetchResource(modelSource, setDownloadProgress);
+        await module.loadModule(fileUri);
+        setIsReady(true);
+      } catch (e) {
+        setError(getError(e));
+      }
+    };
+
+    loadModel();
+  }, [modelSource, module]);
+
+  const forward = async (input: string, classesOfInterest?: string[]) => {
+    if (!isReady) {
+      throw new Error(getError(ETError.ModuleNotLoaded));
+    }
+    if (isGenerating) {
+      throw new Error(getError(ETError.ModelGenerating));
+    }
+
+    try {
+      setIsGenerating(true);
+      const output = await module.forward(input, classesOfInterest || []);
+      return output;
+    } catch (e) {
+      throw new Error(getError(e));
+    } finally {
+      setIsGenerating(false);
+    }
+  };
 
   return { error, isReady, isGenerating, downloadProgress, forward };
 };
