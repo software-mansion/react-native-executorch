3f3a5047852c6cb75bb9c80fc00b00661e72e70d
Jakub Gonera <jakub.gonera@swmansion.com>
2025-03-07 14:32:42 +0100

    Move matrix functionality to utils

--- ios/RnExecutorch/models/image_segmentation/ImageSegmentationModel.h
+++ ios/RnExecutorch/models/image_segmentation/ImageSegmentationModel.h
@@ -7,16 +7,3 @@
                   returnClasses:(NSArray *)classesOfInterest;
 
 @end
-
-template <typename T>
-NSArray* matToNSArray(const cv::Mat& mat) {
-    std::size_t numPixels = mat.rows * mat.cols;
-    NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:numPixels];
-
-    for (std::size_t x = 0; x < mat.rows; ++x) {
-        for (std::size_t y = 0; y < mat.cols; ++y) {
-            arr[x * mat.cols + y] = @(mat.at<T>(x, y));
-        }
-    }
-    return arr;
-}
