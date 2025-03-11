de1d2a56e0163f2f5f8e1402089212417cf831aa
Jakub Gonera <jakub.gonera@swmansion.com>
2025-03-11 11:21:36 +0100

    Add optional resize for Android segmentation

--- android/src/main/java/com/swmansion/rnexecutorch/ImageSegmentation.kt
+++ android/src/main/java/com/swmansion/rnexecutorch/ImageSegmentation.kt
@@ -2,24 +2,23 @@ package com.swmansion.rnexecutorch
 
 import android.util.Log
 import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.WritableMap
-import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReactApplicationContext
-import com.swmansion.rnexecutorch.utils.ETError
+import com.facebook.react.bridge.ReadableArray
 import com.swmansion.rnexecutorch.models.imagesegmentation.ImageSegmentationModel
+import com.swmansion.rnexecutorch.utils.ETError
 import com.swmansion.rnexecutorch.utils.ImageProcessor
 import org.opencv.android.OpenCVLoader
 
-class ImageSegmentation(reactContext: ReactApplicationContext) :
-  NativeImageSegmentationSpec(reactContext) {
-
+class ImageSegmentation(
+  reactContext: ReactApplicationContext,
+) : NativeImageSegmentationSpec(reactContext) {
   private lateinit var model: ImageSegmentationModel
 
   companion object {
     const val NAME = "ImageSegmentation"
 
     init {
-      if(!OpenCVLoader.initLocal()){
+      if (!OpenCVLoader.initLocal()) {
         Log.d("rn_executorch", "OpenCV not loaded")
       } else {
         Log.d("rn_executorch", "OpenCV loaded")
@@ -27,7 +26,10 @@ class ImageSegmentation(reactContext: ReactApplicationContext) :
     }
   }
 
-  override fun loadModule(modelSource: String, promise: Promise) {
+  override fun loadModule(
+    modelSource: String,
+    promise: Promise,
+  ) {
     try {
       model = ImageSegmentationModel(reactApplicationContext)
       model.loadModel(modelSource)
@@ -37,20 +39,20 @@ class ImageSegmentation(reactContext: ReactApplicationContext) :
     }
   }
 
-  override fun forward(input: String, 
-                       classesOfInterest: ReadableArray,
-                       resize:Boolean,
-                       promise: Promise) {
+  override fun forward(
+    input: String,
+    classesOfInterest: ReadableArray,
+    resize: Boolean,
+    promise: Promise,
+  ) {
     try {
       val output =
-          model.runModel(Pair(ImageProcessor.readImage(input), classesOfInterest))
+        model.runModel(Triple(ImageProcessor.readImage(input), classesOfInterest, resize))
       promise.resolve(output)
-    }catch(e: Exception){
+    } catch (e: Exception) {
       promise.reject(e.message!!, e.message)
     }
   }
 
-  override fun getName(): String {
-    return NAME
-  }
+  override fun getName(): String = NAME
 }
