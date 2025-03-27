8ed38040283bd3b11fd1c72ba117864a2903de7a
Norbert Klockiewicz <Nklockiewicz12@gmail.com>
2025-03-27 14:37:43 +0100

    feat: add option to pass skip special tokens flag to decode

--- third-party/ios/ExecutorchLib/ExecutorchLib/Exported/HuggingFaceTokenizer.h
+++ third-party/ios/ExecutorchLib/ExecutorchLib/Exported/HuggingFaceTokenizer.h
@@ -5,6 +5,8 @@
 - (instancetype)initWithTokenizerPath:(NSString *)tokenizerPath;
 - (NSArray<NSNumber *> *)encode:(NSString *)text;
 - (NSString *)decode:(NSArray<NSNumber *> *)tokenIds;
+- (NSString *)decode:(NSArray<NSNumber *> *)tokenIds
+    skipSpecialTokens:(BOOL)skipSpecialTokens;
 - (NSUInteger)getVocabSize;
 - (NSString *)idToToken:(NSInteger)tokenId;
 - (NSInteger)tokenToId:(NSString *)token;
