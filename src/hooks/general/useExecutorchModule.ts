f19605d0ee4ac70f2c9a3fd345303d5c1156a99e
chmjkb <jakubchmura1607@gmail.com>
2025-04-08 10:58:00 +0200

    refactor: add useImageSegmentation, useExecutorchModule

--- src/hooks/general/useExecutorchModule.ts
+++ src/hooks/general/useExecutorchModule.ts
@@ -1,58 +1,9 @@
-import { useState } from 'react';
-import { _ETModule } from '../../native/RnExecutorchModules';
-import { useModule } from '../useModule';
-import { ETInput } from '../../types/common';
-import { getError } from '../../Error';
+import { ExecutorchModule } from '../../modules/general/ExecutorchModule';
+import { useModule2 } from '../useModule2';
 
 interface Props {
   modelSource: string | number;
 }
 
-export const useExecutorchModule = ({
-  modelSource,
-}: Props): {
-  error: string | null;
-  isReady: boolean;
-  isGenerating: boolean;
-  downloadProgress: number;
-  forward: (
-    input: ETInput | ETInput[],
-    shape: number[] | number[][]
-  ) => Promise<number[][]>;
-  loadMethod: (methodName: string) => Promise<void>;
-  loadForward: () => Promise<void>;
-} => {
-  const [module] = useState(() => new _ETModule());
-  const {
-    error,
-    isReady,
-    isGenerating,
-    downloadProgress,
-    forwardETInput: forward,
-  } = useModule({
-    modelSource,
-    module,
-  });
-
-  const loadMethod = async (methodName: string) => {
-    try {
-      await module.loadMethod(methodName);
-    } catch (e) {
-      throw new Error(getError(e));
-    }
-  };
-
-  const loadForward = async () => {
-    await loadMethod('forward');
-  };
-
-  return {
-    error,
-    isReady,
-    isGenerating,
-    downloadProgress,
-    forward,
-    loadMethod,
-    loadForward,
-  };
-};
+export const useExecutorchModule = ({ modelSource }: Props) =>
+  useModule2({ module: ExecutorchModule, loadArgs: [modelSource] });
