4e502787f5a4847ec459b17c69cdc7d3691e83ae
pweglik <przemek.weglik@onet.pl>
2025-04-15 16:38:00 +0200

    Add llama tokenizer config

--- src/constants/modelUrls.ts
+++ src/constants/modelUrls.ts
@@ -13,10 +13,10 @@ export const LLAMA3_2_1B_QLORA =
   'https://huggingface.co/software-mansion/react-native-executorch-llama-3.2/resolve/v0.4.0/llama-3.2-1B/QLoRA/llama3_2_qat_lora.pte';
 export const LLAMA3_2_1B_SPINQUANT =
   'https://huggingface.co/software-mansion/react-native-executorch-llama-3.2/resolve/v0.4.0/llama-3.2-1B/spinquant/llama3_2_spinquant.pte';
-export const LLAMA3_2_1B_TOKENIZER =
+export const LLAMA3_2_TOKENIZER =
   'https://huggingface.co/software-mansion/react-native-executorch-llama-3.2/resolve/v0.4.0/tokenizer.json';
-export const LLAMA3_2_3B_TOKENIZER =
-  'https://huggingface.co/software-mansion/react-native-executorch-llama-3.2/resolve/v0.4.0/tokenizer.json';
+export const LLAMA3_2_TOKENIZER_CONFIG =
+  'https://huggingface.co/software-mansion/react-native-executorch-llama-3.2/resolve/v0.4.0/tokenizer_config.json';
 
 // Classification
 export const EFFICIENTNET_V2_S =
