4e7f121d17d8086deaed444ada6a7eec54a82775
Michał Dydek <54865962+mdydek@users.noreply.github.com>
2025-04-22 13:56:17 +0200

    feat: llama with whisper demo (#179)
    ## Description
    
    <!-- Provide a concise and descriptive summary of the changes
    implemented in this PR. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    
    ### Tested on
    
    - [x] iOS
    - [x] Android
    
    ### Testing instructions
    
    <!-- Provide step-by-step instructions on how to test your changes.
    Include setup details if necessary. -->
    
    ### Screenshots
    
    <!-- Add screenshots here, if applicable -->
    
    ### Related issues
    
    <!-- Link related issues here using #issue-number -->
    
    ### Checklist
    
    - [x] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have updated the documentation accordingly
    - [ ] My changes generate no new warnings
    
    ### Additional notes
    
    <!-- Include any additional information, assumptions, or context that
    reviewers might need to understand this PR. -->

--- examples/llama/screens/ChatScreen.tsx
+++ examples/llama/screens/ChatScreen.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
+import { STREAMING_ACTION, useSpeechToText } from 'react-native-executorch';
 import {
   Keyboard,
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
   Text,
-  TextInput,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
+  TextInput,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SWMIcon from '../assets/icons/swm_icon.svg';
-import SendIcon from '../assets/icons/send_icon.svg';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {
   LLAMA3_2_1B_QLORA,
@@ -20,45 +20,137 @@ import {
   useLLM,
 } from 'react-native-executorch';
 import PauseIcon from '../assets/icons/pause_icon.svg';
+import MicIcon from '../assets/icons/mic_icon.svg';
+import SendIcon from '../assets/icons/send_icon.svg';
+import StopIcon from '../assets/icons/stop_icon.svg';
 import ColorPalette from '../colors';
 import Messages from '../components/Messages';
 import { MessageType, SenderType } from '../types';
+import LiveAudioStream from 'react-native-live-audio-stream';
+import { Buffer } from 'buffer';
+import * as Speech from 'expo-speech';
+
+const audioStreamOptions = {
+  sampleRate: 16000,
+  channels: 1,
+  bitsPerSample: 16,
+  audioSource: 1,
+  bufferSize: 16000,
+};
+
+const startStreamingAudio = (options: any, onChunk: (data: string) => void) => {
+  LiveAudioStream.init(options);
+  LiveAudioStream.on('data', onChunk);
+  LiveAudioStream.start();
+};
+
+const float32ArrayFromPCMBinaryBuffer = (b64EncodedBuffer: string) => {
+  const b64DecodedChunk = Buffer.from(b64EncodedBuffer, 'base64');
+  const int16Array = new Int16Array(b64DecodedChunk.buffer);
+
+  const float32Array = new Float32Array(int16Array.length);
+  for (let i = 0; i < int16Array.length; i++) {
+    float32Array[i] = Math.max(
+      -1,
+      Math.min(1, (int16Array[i] / audioStreamOptions.bufferSize) * 8)
+    );
+  }
+  return float32Array;
+};
 
 export default function ChatScreen() {
   const [chatHistory, setChatHistory] = useState<Array<MessageType>>([]);
+  const [isRecording, setIsRecording] = useState(false);
   const [isTextInputFocused, setIsTextInputFocused] = useState(false);
   const [userInput, setUserInput] = useState('');
+  const textInputRef = useRef<TextInput>(null);
+  const messageRecorded = useRef<boolean>(false);
   const llama = useLLM({
     modelSource: LLAMA3_2_1B_QLORA,
     tokenizerSource: LLAMA3_2_1B_TOKENIZER,
     contextWindowLength: 6,
   });
-  const textInputRef = useRef<TextInput>(null);
+  const speechToText = useSpeechToText({
+    modelName: 'whisper',
+    windowSize: 5,
+    overlapSeconds: 1.2,
+  });
+
+  const onChunk = (data: string) => {
+    const float32Chunk = float32ArrayFromPCMBinaryBuffer(data);
+    speechToText.streamingTranscribe(
+      STREAMING_ACTION.DATA,
+      Array.from(float32Chunk)
+    );
+  };
+
+  const handleRecordPress = async () => {
+    if (isRecording) {
+      setIsRecording(false);
+      LiveAudioStream.stop();
+      messageRecorded.current = true;
+      await llama.generate(
+        await speechToText.streamingTranscribe(STREAMING_ACTION.STOP)
+      );
+    } else {
+      setIsRecording(true);
+      startStreamingAudio(audioStreamOptions, onChunk);
+      speechToText.streamingTranscribe(STREAMING_ACTION.START);
+    }
+  };
+
+  const handleSendPress = async () => {
+    if (userInput) {
+      messageRecorded.current = false;
+      appendToMessageHistory(userInput, 'user');
+      setUserInput('');
+      setIsTextInputFocused(false);
+      textInputRef.current?.clear();
+      try {
+        if (!llama.isGenerating) await llama.generate(userInput);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
   useEffect(() => {
     if (llama.response && !llama.isGenerating) {
       appendToMessageHistory(llama.response, 'assistant');
     }
   }, [llama.response, llama.isGenerating]);
 
-  const appendToMessageHistory = (content: string, role: SenderType) => {
-    setChatHistory((prevHistory) => [...prevHistory, { role, content }]);
-  };
-
-  const sendMessage = async () => {
-    appendToMessageHistory(userInput.trim(), 'user');
-    setUserInput('');
-    textInputRef.current?.clear();
-    try {
-      await llama.generate(userInput);
-    } catch (e) {
-      console.error(e);
+  const modifyLastMessage = (content: string) => {
+    const lastMessage = chatHistory[chatHistory.length - 1];
+    if (lastMessage && lastMessage.role === 'user') {
+      setChatHistory((prevHistory) => [
+        ...prevHistory.slice(0, -1),
+        { ...lastMessage, content },
+      ]);
+    } else {
+      appendToMessageHistory(content, 'user');
     }
   };
 
-  return !llama.isReady ? (
+  useEffect(() => {
+    if (chatHistory.length === 0 && !speechToText.isGenerating) return;
+    if (speechToText.isGenerating) {
+      if (speechToText.sequence) modifyLastMessage(speechToText.sequence);
+      else modifyLastMessage('...');
+    } else modifyLastMessage(speechToText.sequence);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [speechToText.sequence, speechToText.isGenerating]);
+
+  const appendToMessageHistory = (content: string, role: SenderType) => {
+    setChatHistory((prevHistory) => [...prevHistory, { role, content }]);
+    if (role == 'assistant' && messageRecorded.current)
+      Speech.speak(content, { language: 'en-US' });
+  };
+
+  return !llama.isReady || !speechToText.isReady ? (
     <Spinner
-      visible={!llama.isReady}
-      textContent={`Loading the model ${(llama.downloadProgress * 100).toFixed(0)} %`}
+      visible={!llama.isReady || !speechToText.isReady}
+      textContent={`Loading the model ${(llama.downloadProgress * 100).toFixed(0)} %\nLoading the speech model ${(speechToText.downloadProgress * 100).toFixed(0)} %`}
     />
   ) : (
     <SafeAreaView style={styles.container}>
@@ -70,7 +162,9 @@ export default function ChatScreen() {
         >
           <View style={styles.topContainer}>
             <SWMIcon width={45} height={45} />
-            <Text style={styles.textModelName}>Llama 3.2 1B QLoRA</Text>
+            <Text style={styles.textModelName}>
+              Llama 3.2 1B QLoRA x Whisper
+            </Text>
           </View>
           {chatHistory.length ? (
             <View style={styles.chatContainer}>
@@ -93,11 +187,13 @@ export default function ChatScreen() {
             <TextInput
               onFocus={() => setIsTextInputFocused(true)}
               onBlur={() => setIsTextInputFocused(false)}
+              editable={!isRecording && !llama.isGenerating}
               style={{
                 ...styles.textInput,
                 borderColor: isTextInputFocused
                   ? ColorPalette.blueDark
                   : ColorPalette.blueLight,
+                display: isRecording ? 'none' : 'flex',
               }}
               placeholder="Your message"
               placeholderTextColor={'#C1C6E5'}
@@ -105,22 +201,29 @@ export default function ChatScreen() {
               ref={textInputRef}
               onChangeText={(text: string) => setUserInput(text)}
             />
-            {userInput && (
-              <TouchableOpacity
-                style={styles.sendChatTouchable}
-                onPress={async () =>
-                  !llama.isGenerating && (await sendMessage())
-                }
-              >
-                <SendIcon height={24} width={24} padding={4} margin={8} />
+            {llama.isGenerating ? (
+              <TouchableOpacity onPress={llama.interrupt}>
+                <PauseIcon height={40} width={40} padding={4} margin={8} />
               </TouchableOpacity>
-            )}
-            {llama.isGenerating && (
+            ) : !userInput ? (
               <TouchableOpacity
-                style={styles.sendChatTouchable}
-                onPress={llama.interrupt}
+                style={
+                  !isRecording ? styles.recordTouchable : styles.recordingInfo
+                }
+                onPress={handleRecordPress}
               >
-                <PauseIcon height={24} width={24} padding={4} margin={8} />
+                {isRecording ? (
+                  <StopIcon height={40} width={40} padding={4} margin={8} />
+                ) : (
+                  <MicIcon height={40} width={40} padding={4} margin={8} />
+                )}
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={styles.recordTouchable}
+                onPress={handleSendPress}
+              >
+                <SendIcon height={40} width={40} padding={4} margin={8} />
               </TouchableOpacity>
             )}
           </View>
@@ -185,10 +288,20 @@ const styles = StyleSheet.create({
     color: ColorPalette.primary,
     padding: 16,
   },
-  sendChatTouchable: {
+  fromUrlTouchable: {
     height: '100%',
-    width: 48,
     justifyContent: 'center',
-    alignItems: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  recordTouchable: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recordingInfo: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
