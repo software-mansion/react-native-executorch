6567e87cd9a2c4332c99563675a1ea8bbbdd36ee
Jakub Gonera <jakub.gonera@swmansion.com>
2025-05-15 10:54:49 +0200

    fix: address the rest of the comments

--- common/rnexecutorch/data_processing/base64.cpp
+++ common/rnexecutorch/data_processing/base64.cpp
@@ -26,15 +26,17 @@ std::string base64_encode(BYTE const *buf, unsigned int bufLen) {
           ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
       char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for (i = 0; (i < 4); i++)
+      for (i = 0; (i < 4); i++) {
         ret += base64_chars[char_array_4[i]];
+      }
       i = 0;
     }
   }
 
   if (i) {
-    for (j = i; j < 3; j++)
+    for (j = i; j < 3; j++) {
       char_array_3[j] = '\0';
+    }
 
     char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
     char_array_4[1] =
@@ -43,11 +45,13 @@ std::string base64_encode(BYTE const *buf, unsigned int bufLen) {
         ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
     char_array_4[3] = char_array_3[2] & 0x3f;
 
-    for (j = 0; (j < i + 1); j++)
+    for (j = 0; (j < i + 1); j++) {
       ret += base64_chars[char_array_4[j]];
+    }
 
-    while ((i++ < 3))
+    while ((i++ < 3)) {
       ret += '=';
+    }
   }
 
   return ret;
@@ -66,8 +70,9 @@ std::vector<BYTE> base64_decode(std::string const &encoded_string) {
     char_array_4[i++] = encoded_string[in_];
     in_++;
     if (i == 4) {
-      for (i = 0; i < 4; i++)
+      for (i = 0; i < 4; i++) {
         char_array_4[i] = base64_chars.find(char_array_4[i]);
+      }
 
       char_array_3[0] =
           (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
@@ -75,26 +80,30 @@ std::vector<BYTE> base64_decode(std::string const &encoded_string) {
           ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
       char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-      for (i = 0; (i < 3); i++)
+      for (i = 0; (i < 3); i++) {
         ret.push_back(char_array_3[i]);
+      }
       i = 0;
     }
   }
 
   if (i) {
-    for (j = i; j < 4; j++)
+    for (j = i; j < 4; j++) {
       char_array_4[j] = 0;
+    }
 
-    for (j = 0; j < 4; j++)
+    for (j = 0; j < 4; j++) {
       char_array_4[j] = base64_chars.find(char_array_4[j]);
+    }
 
     char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
     char_array_3[1] =
         ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
     char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-    for (j = 0; (j < i - 1); j++)
+    for (j = 0; (j < i - 1); j++) {
       ret.push_back(char_array_3[j]);
+    }
   }
 
   return ret;
