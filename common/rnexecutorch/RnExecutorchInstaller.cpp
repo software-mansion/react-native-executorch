64eb709ab2a99e506147be88fb0d27f32e6c74c4
Jakub Gonera <38468585+JakubGonera@users.noreply.github.com>
2025-05-21 16:06:31 +0200

    fix: generic loading of host functions in C++ (#315)
    ## Description
    
    Add generic loading of host functions in C++.
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    
    ### Tested on
    
    - [x] iOS
    - [x] Android

--- common/rnexecutorch/RnExecutorchInstaller.cpp
+++ common/rnexecutorch/RnExecutorchInstaller.cpp
@@ -1,8 +1,7 @@
 #include "RnExecutorchInstaller.h"
 
 #include <rnexecutorch/host_objects/JsiConversions.h>
-#include <rnexecutorch/host_objects/ModelHostObject.h>
-#include <rnexecutorch/models/StyleTransfer.h>
+#include <rnexecutorch/models/style_transfer/StyleTransfer.h>
 
 namespace rnexecutorch {
 
@@ -11,50 +10,14 @@ namespace rnexecutorch {
 // SSL intricacies manually, as it is done automagically in ObjC++/Kotlin.
 FetchUrlFunc_t fetchUrlFunc;
 
-jsi::Function RnExecutorchInstaller::loadStyleTransfer(
-    jsi::Runtime *jsiRuntime,
-    const std::shared_ptr<react::CallInvoker> &jsCallInvoker) {
-  return jsi::Function::createFromHostFunction(
-      *jsiRuntime, jsi::PropNameID::forAscii(*jsiRuntime, "loadStyleTransfer"),
-      0,
-      [jsCallInvoker](jsi::Runtime &runtime, const jsi::Value &thisValue,
-                      const jsi::Value *args, size_t count) -> jsi::Value {
-        assert(count == 1);
-        try {
-          auto source = jsiconversion::getValue<std::string>(args[0], runtime);
-
-          auto styleTransferPtr =
-              std::make_shared<StyleTransfer>(source, &runtime);
-          auto styleTransferHostObject =
-              std::make_shared<ModelHostObject<StyleTransfer>>(styleTransferPtr,
-                                                               jsCallInvoker);
-
-          return jsi::Object::createFromHostObject(runtime,
-                                                   styleTransferHostObject);
-        } catch (const std::runtime_error &e) {
-          // This catch should be merged with the next one
-          // (std::runtime_error inherits from std::exception) HOWEVER react
-          // native has broken RTTI which breaks proper exception type
-          // checking. Remove when the following change is present in our
-          // version:
-          // https://github.com/facebook/react-native/commit/3132cc88dd46f95898a756456bebeeb6c248f20e
-          throw jsi::JSError(runtime, e.what());
-        } catch (const std::exception &e) {
-          throw jsi::JSError(runtime, e.what());
-        } catch (...) {
-          throw jsi::JSError(runtime, "Unknown error");
-        }
-      });
-}
-
 void RnExecutorchInstaller::injectJSIBindings(
-    jsi::Runtime *jsiRuntime,
-    const std::shared_ptr<react::CallInvoker> &jsCallInvoker,
+    jsi::Runtime *jsiRuntime, std::shared_ptr<react::CallInvoker> jsCallInvoker,
     FetchUrlFunc_t fetchDataFromUrl) {
   fetchUrlFunc = fetchDataFromUrl;
 
   jsiRuntime->global().setProperty(
       *jsiRuntime, "loadStyleTransfer",
-      loadStyleTransfer(jsiRuntime, jsCallInvoker));
+      RnExecutorchInstaller::loadModel<StyleTransfer>(jsiRuntime, jsCallInvoker,
+                                                      "loadStyleTransfer"));
 }
 } // namespace rnexecutorch
--- common/rnexecutorch/RnExecutorchInstaller.h
+++ common/rnexecutorch/RnExecutorchInstaller.h
@@ -7,9 +7,13 @@
 #include <ReactCommon/CallInvoker.h>
 #include <jsi/jsi.h>
 
+#include <rnexecutorch/host_objects/JsiConversions.h>
+#include <rnexecutorch/host_objects/ModelHostObject.h>
+
 namespace rnexecutorch {
 
 using FetchUrlFunc_t = std::function<std::vector<std::byte>(std::string)>;
+extern FetchUrlFunc_t fetchUrlFunc;
 
 using namespace facebook;
 
@@ -17,13 +21,57 @@ class RnExecutorchInstaller {
 public:
   static void
   injectJSIBindings(jsi::Runtime *jsiRuntime,
-                    const std::shared_ptr<react::CallInvoker> &jsCallInvoker,
+                    std::shared_ptr<react::CallInvoker> jsCallInvoker,
                     FetchUrlFunc_t fetchDataFromUrl);
 
 private:
+  template <typename ModelT>
   static jsi::Function
-  loadStyleTransfer(jsi::Runtime *jsiRuntime,
-                    const std::shared_ptr<react::CallInvoker> &jsCallInvoker);
+  loadModel(jsi::Runtime *jsiRuntime,
+            std::shared_ptr<react::CallInvoker> jsCallInvoker,
+            const std::string &loadFunctionName) {
+
+    return jsi::Function::createFromHostFunction(
+        *jsiRuntime, jsi::PropNameID::forAscii(*jsiRuntime, loadFunctionName),
+        0,
+        [jsCallInvoker](jsi::Runtime &runtime, const jsi::Value &thisValue,
+                        const jsi::Value *args, size_t count) -> jsi::Value {
+          if (count != 1) {
+            char errorMessage[100];
+            std::snprintf(
+                errorMessage, sizeof(errorMessage),
+                "Argument count mismatch, was expecting: 1 but got: %zu",
+                count);
+            throw jsi::JSError(runtime, errorMessage);
+          }
+          try {
+            auto source =
+                jsiconversion::getValue<std::string>(args[0], runtime);
+
+            auto modelImplementationPtr =
+                std::make_shared<ModelT>(source, &runtime);
+            auto modelHostObject = std::make_shared<ModelHostObject<ModelT>>(
+                modelImplementationPtr, jsCallInvoker);
+
+            return jsi::Object::createFromHostObject(runtime, modelHostObject);
+          } catch (const std::runtime_error &e) {
+            // This catch should be merged with the next one
+            // (std::runtime_error inherits from std::exception) HOWEVER react
+            // native has broken RTTI which breaks proper exception type
+            // checking. Remove when the following change is present in our
+            // version:
+            // https://github.com/facebook/react-native/commit/3132cc88dd46f95898a756456bebeeb6c248f20e
+            throw jsi::JSError(runtime, e.what());
+            return jsi::Value();
+          } catch (const std::exception &e) {
+            throw jsi::JSError(runtime, e.what());
+            return jsi::Value();
+          } catch (...) {
+            throw jsi::JSError(runtime, "Unknown error");
+            return jsi::Value();
+          }
+        });
+  }
 };
 
 } // namespace rnexecutorch
