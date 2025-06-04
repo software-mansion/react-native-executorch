7a7ad3fd668ea133ea570494631833ce3a19ce72
Jakub Gonera <jakub.gonera@swmansion.com>
2025-06-04 12:59:33 +0200

    fix: assert model input tensor count

--- packages/react-native-executorch/common/rnexecutorch/models/classification/Classification.cpp
+++ packages/react-native-executorch/common/rnexecutorch/models/classification/Classification.cpp
@@ -11,7 +11,19 @@ namespace rnexecutorch {
 Classification::Classification(const std::string &modelSource,
                                std::shared_ptr<react::CallInvoker> callInvoker)
     : BaseModel(modelSource, callInvoker) {
-  std::vector<int32_t> modelInputShape = getInputShape()[0];
+  auto inputTensors = getInputShape();
+  if (inputTensors.size() == 0) {
+    throw std::runtime_error("Model seems to not take any input tensors.");
+  }
+  std::vector<int32_t> modelInputShape = inputTensors[0];
+  if (modelInputShape.size() < 2) {
+    char errorMessage[100];
+    std::snprintf(errorMessage, sizeof(errorMessage),
+                  "Unexpected model input size, expected at least 2 dimentions "
+                  "but got: %zu.",
+                  modelInputShape.size());
+    throw std::runtime_error(errorMessage);
+  }
   modelImageSize = cv::Size(modelInputShape[modelInputShape.size() - 1],
                             modelInputShape[modelInputShape.size() - 2]);
 }
@@ -32,7 +44,6 @@ Classification::forward(std::string imageSource) {
 
 TensorPtr Classification::preprocess(const std::string &imageSource) {
   cv::Mat image = imageprocessing::readImage(imageSource);
-  auto originalSize = image.size();
   cv::resize(image, image, modelImageSize);
 
   return imageprocessing::getTensorFromMatrix(getInputShape()[0], image);
