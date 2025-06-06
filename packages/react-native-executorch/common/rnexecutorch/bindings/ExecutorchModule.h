b1afabad618a8a77c3c4eca178ad8738c368d257
chmjkb <jakubchmura1607@gmail.com>
2025-06-06 11:39:32 +0200

    fix: update ExecutorchModule header

--- packages/react-native-executorch/common/rnexecutorch/bindings/ExecutorchModule.h
+++ packages/react-native-executorch/common/rnexecutorch/bindings/ExecutorchModule.h
@@ -5,7 +5,10 @@
 #include <ReactCommon/CallInvoker.h>
 #include <executorch/extension/module/module.h>
 #include <jsi/jsi.h>
+#include <rnexecutorch/jsi/OwningArrayBuffer.h>
 #include <rnexecutorch/utils/JsiTensorView.h>
+
+using namespace facebook;
 
 namespace rnexecutorch {
 
@@ -14,7 +17,8 @@ public:
   ExecutorchModule(const std::string &modelSource,
                    std::shared_ptr<facebook::react::CallInvoker> callInvoker);
   std::vector<int32_t> getInputShape(std::string method_name, int index);
-  int forward(std::vector<JsiTensorView> tensorViewVec);
+  std::vector<std::shared_ptr<OwningArrayBuffer>>
+  forward(std::vector<JsiTensorView> tensorViewVec);
 
 protected:
   std::unique_ptr<executorch::extension::Module> module;
