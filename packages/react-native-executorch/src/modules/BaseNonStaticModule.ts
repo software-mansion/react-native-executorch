af6cd0ed642b643da2c56a1746bcb69815b07a73
chmjkb <jakubchmura1607@gmail.com>
2025-06-09 14:50:39 +0200

    feat: make basenonstaticmodule able to call all the functions from BaseModel.cpp

--- packages/react-native-executorch/src/modules/BaseNonStaticModule.ts
+++ packages/react-native-executorch/src/modules/BaseNonStaticModule.ts
@@ -1,5 +1,29 @@
+import { ResourceSource } from '../types/common';
+import { ResourceFetcher } from '../utils/ResourceFetcher';
+import { TensorPtr } from '../types/common';
+
 export class BaseNonStaticModule {
   nativeModule: any = null;
+
+  async load(
+    modelSource: ResourceSource,
+    onDownloadProgressCallback: (_: number) => void = () => {}
+  ): Promise<void> {
+    const paths = await ResourceFetcher.fetchMultipleResources(
+      onDownloadProgressCallback,
+      modelSource
+    );
+    this.nativeModule = global.loadExecutorchModule(paths[0] || '');
+  }
+
+  protected async forwardET(inputTensor: TensorPtr[]): Promise<ArrayBuffer[]> {
+    return await this.nativeModule.forward(inputTensor);
+  }
+
+  async getInputShape(methodName: string, index: number): Promise<number[]> {
+    return this.nativeModule.getInputShape(methodName, index);
+  }
+
   delete() {
     if (this.nativeModule !== null) {
       this.nativeModule.unload();
