211757d533702ce477371ab5dfaa0d433f09b682
Jakub Gonera <38468585+JakubGonera@users.noreply.github.com>
2025-06-10 10:30:47 +0200

    feat: port object detection to C++ (#361)
    ## Description
    
    Port object detection native code to C++
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    
    ### Tested on
    
    - [x] iOS
    - [x] Android
    
    ### Related issues
    
    #260 #255
    
    ### Checklist
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have updated the documentation accordingly
    - [x] My changes generate no new warnings

---
+++ packages/react-native-executorch/common/rnexecutorch/models/object_detection/Utils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+namespace rnexecutorch {
+struct Detection {
+  float x1;
+  float y1;
+  float x2;
+  float y2;
+  int label;
+  float score;
+};
+
+inline constexpr float iouThreshold = 0.55;
+
+float intersectionOverUnion(const Detection &a, const Detection &b);
+std::vector<Detection> nonMaxSuppression(std::vector<Detection> detections);
+} // namespace rnexecutorch
