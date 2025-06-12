2286660ead5274830c40979e445eec2db30f0199
Jakub Gonera <jakub.gonera@swmansion.com>
2025-06-12 12:25:41 +0200

    feat: add OCR scaffolding

--- packages/react-native-executorch/common/rnexecutorch/models/ocr/OCR.cpp
+++ packages/react-native-executorch/common/rnexecutorch/models/ocr/OCR.cpp
@@ -1,3 +1,31 @@
 #include "OCR.h"
 
-namespace rnexecutorch {} // namespace rnexecutorch
+#include <rnexecutorch/models/ocr/Constants.h>
+
+namespace rnexecutorch {
+OCR::OCR(std::string detectorSource, std::string recognizerSourceLarge,
+         std::string recognizerSourceMedium, std::string recognizerSourceSmall,
+         std::string symbols, std::shared_ptr<react::CallInvoker> callInvoker)
+    : detector(detectorSource, callInvoker),
+      recognitionHandler(symbols, recognizerSourceLarge, recognizerSourceMedium,
+                         recognizerSourceSmall, callInvoker) {}
+
+std::vector<OCRDetection> OCR::forward(std::string input) {
+  std::vector<DetectorBBox> detectorResult = detector.forward(input);
+  cv::Mat grayscaleImg = imageprocessing::readImage(input);
+  cv::cvtColor(grayscaleImg, grayscaleImg, cv::COLOR_BGR2GRAY);
+  std::vector<OCRDetection> result = recognitionHandler.recognize(
+      detectorResult, grayscaleImg, recognizerImageSize, recognizerImageSize);
+  return result;
+}
+
+void OCR::unloadModule() {
+  detector.unload();
+  recognitionHandler.unload();
+}
+
+std::size_t OCR::getMemoryLowerBound() {
+  return detector.getMemoryLowerBound() +
+         recognitionHandler.getMemoryLowerBound();
+}
+} // namespace rnexecutorch
