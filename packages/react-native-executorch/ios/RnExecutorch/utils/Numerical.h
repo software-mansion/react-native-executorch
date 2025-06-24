15d864a4219bd5a482a3a34e1569e48ba74cd2f0
chmjkb <jakubchmura1607@gmail.com>
2025-06-24 11:43:55 +0200

    fix: remove accidental changes

--- packages/react-native-executorch/ios/RnExecutorch/utils/Numerical.h
+++ packages/react-native-executorch/ios/RnExecutorch/utils/Numerical.h
@@ -1,6 +1,3 @@
 #include <vector>
 
 std::vector<double> softmax(const std::vector<double> &v);
-void normalize(std::vector<float> &v);
-std::vector<float> meanPooling(std::vector<float> &modelOutput,
-                               std::vector<float> &attnMask);
