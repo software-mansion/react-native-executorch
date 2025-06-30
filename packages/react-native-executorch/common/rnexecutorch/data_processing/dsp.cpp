3b2fc402c27a5aadb077a08a98daeacbdfec4415
chmjkb <jakubchmura1607@gmail.com>
2025-06-30 13:01:34 +0200

    chore: remove redundant comment

--- packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.cpp
+++ packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.cpp
@@ -9,7 +9,7 @@ namespace rnexecutorch::dsp {
 /// @brief Generates Hann Window coefficients:
 /// https://www.mathworks.com/help/signal/ref/hann.html
 std::vector<float> hann_window(std::size_t size) {
-  std::vector<float> hann(size); // ✅ Create elements
+  std::vector<float> hann(size);
 
   for (std::size_t i = 0; i < size; i++) {
     hann[i] = 0.5f * (1.0f - std::cos(2.0f * M_PI * i / (size - 1)));
