055cff24671116d53e55ca1e46f6bbacbf5fd3d4
Mateusz Sluszniak <sluszmat@amazon.com>
2025-07-08 09:08:12 +0200

    Improve testing using CMakeLists.txt

--- packages/react-native-executorch/common/rnexecutorch/data_processing/Numerical.cpp
+++ packages/react-native-executorch/common/rnexecutorch/data_processing/Numerical.cpp
@@ -41,45 +41,32 @@ void normalize(std::span<float> input) {
   }
 }
 
-template void softmax(std::vector<float> &);
-template void softmax(std::vector<double> &);
-template void softmax(std::span<float>);
-template void softmax(std::span<double>);
-
-template <typename Container> void normalize(Container &input) {
-  static_assert(
-      std::is_floating_point_v<typename Container::value_type>,
-      "Standardization requires a container with floating-point type.");
-
-  using ValueType = typename Container::value_type;
+void normalize(std::span<float> input) {
 
   if (input.empty()) {
     return;
   }
 
-  constexpr auto zeroValueType = static_cast<ValueType>(0);
-  const ValueType mean =
-      std::accumulate(std::begin(input), std::end(input), zeroValueType) /
-      input.size();
-  const ValueType squaredSum = std::inner_product(
-      std::begin(input), std::end(input), std::begin(input), zeroValueType);
-  const ValueType variance = (squaredSum / input.size()) - (mean * mean);
-  const ValueType standardDeviation = std::sqrt(variance);
+  const auto mean =
+      std::accumulate(std::begin(input), std::end(input), 0.0F) / input.size();
+  const auto squaredSum = std::inner_product(std::begin(input), std::end(input),
+                                             std::begin(input), 0.0F);
+  const auto variance = (squaredSum / input.size()) - (mean * mean);
+  const auto standardDeviation = std::sqrt(variance);
 
-  if (standardDeviation == zeroValueType) {
-    return; // Prevent division by zero if all elements are the same
+  const auto epsilon = std::numeric_limits<float>::epsilon();
+  // If standard deviation is extremely small return zero vector
+  // This prevents dividing by almost zero values
+  if (standardDeviation < epsilon) {
+    std::fill(input.begin(), input.end(), 0.0F);
+    return;
   }
 
   std::transform(std::begin(input), std::end(input), std::begin(input),
-                 [mean, standardDeviation](ValueType value) {
+                 [mean, standardDeviation](float value) {
                    return (value - mean) / standardDeviation;
                  });
 }
-
-template void normalize(std::vector<float> &);
-template void normalize(std::vector<double> &);
-template void normalize(std::span<float>);
-template void normalize(std::span<double>);
 
 std::vector<float> meanPooling(std::span<const float> modelOutput,
                                std::span<const int64_t> attnMask) {
