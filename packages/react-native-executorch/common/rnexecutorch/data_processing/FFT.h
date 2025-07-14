bb53954a2e17c8e9751180f4dee9108bca539605
chmjkb <jakubchmura1607@gmail.com>
2025-07-14 13:56:31 +0200

    chore: remove redundant function declaration

--- packages/react-native-executorch/common/rnexecutorch/data_processing/FFT.h
+++ packages/react-native-executorch/common/rnexecutorch/data_processing/FFT.h
@@ -12,9 +12,7 @@ class FFT {
 public:
   explicit FFT(int size);
   ~FFT();
-
   void doFFT(float *in, std::vector<std::complex<float>> &out);
-  void doInverseFFT(std::vector<std::complex<float>> &in, float *out);
 
 private:
   int size_;
