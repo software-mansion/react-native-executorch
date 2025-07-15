a670118737ba13f72c0223c8dc2d5b0994536c8a
mlodyjesienin <filipzielinski377@gmail.com>
2025-07-14 23:16:28 -0600

    port OCR module to cpp (unclean)

--- packages/react-native-executorch/common/rnexecutorch/models/ocr/OCR.cpp
+++ packages/react-native-executorch/common/rnexecutorch/models/ocr/OCR.cpp
@@ -1,25 +1,30 @@
 #include "OCR.h"
 
+#include <rnexecutorch/Log.h>
+#include <rnexecutorch/data_processing/ImageProcessing.h>
 #include <rnexecutorch/models/ocr/Constants.h>
-
 namespace rnexecutorch {
 OCR::OCR(std::string detectorSource, std::string recognizerSourceLarge,
          std::string recognizerSourceMedium, std::string recognizerSourceSmall,
          std::string symbols, std::shared_ptr<react::CallInvoker> callInvoker)
     : detector(detectorSource, callInvoker),
       recognitionHandler(symbols, recognizerSourceLarge, recognizerSourceMedium,
-                         recognizerSourceSmall, callInvoker) {}
+                         recognizerSourceSmall, callInvoker) {
+  log(LOG_LEVEL::Info, "running!");
+}
 
-std::vector<OCRDetection> OCR::forward(std::string input) {
-  std::vector<DetectorBBox> detectorResult = detector.forward(input);
+std::vector<OCRDetection> OCR::generate(std::string input) {
+  log(LOG_LEVEL::Info, "generating!");
+  std::vector<DetectorBBox> detectorResult = detector.generate(input);
   cv::Mat grayscaleImg = imageprocessing::readImage(input);
   cv::cvtColor(grayscaleImg, grayscaleImg, cv::COLOR_BGR2GRAY);
   std::vector<OCRDetection> result = recognitionHandler.recognize(
-      detectorResult, grayscaleImg, recognizerImageSize, recognizerImageSize);
+      detectorResult, grayscaleImg,
+      cv::Size(ocr::recognizerImageSize, ocr::recognizerImageSize));
   return result;
 }
 
-void OCR::unloadModule() {
+void OCR::unload() {
   detector.unload();
   recognitionHandler.unload();
 }
