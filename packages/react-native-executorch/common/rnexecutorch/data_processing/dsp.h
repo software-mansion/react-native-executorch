354d2acafcb20b65488ce6f1e8494a835a77a053
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2025-08-28 19:00:05 +0200

    Remove declaration of hannWindow
    Signed-off-by: Mateusz Kopciński <mateusz.kopcinski@swmansnion.com>

--- packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.h
+++ packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.h
@@ -5,7 +5,6 @@
 
 namespace rnexecutorch::dsp {
 
-std::vector<float> hannWindow(size_t size);
 std::vector<float> stftFromWaveform(std::span<const float> waveform,
                                     size_t fftWindowSize, size_t hopSize);
 
