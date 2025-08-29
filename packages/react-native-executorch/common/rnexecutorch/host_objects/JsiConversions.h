b2a6d2b23fb8d7590bf993d3103094887fbc174d
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2025-08-29 10:54:44 +0200

    Refactor jsi conversion & NITs (#562)
    ## Description
    
    As in the title, some refactor of jsi.
    
    ### Introduces a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ### Type of change
    
    - [ ] Bug fix (change which fixes an issue)
    - [ ] New feature (change which adds functionality)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    - [x] Other (chores, tests, code style improvements etc.)
    
    ### Tested on
    
    - [ ] iOS
    - [ ] Android
    
    ### Testing instructions
    
    Needs to be manually testes / unit test (TODO)
    
    ### Screenshots
    
    <!-- Add screenshots here, if applicable -->
    
    ### Related issues
    
    <!-- Link related issues here using #issue-number -->
    
    ### Checklist
    
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have updated the documentation accordingly
    - [ ] My changes generate no new warnings
    
    ### Additional notes
    
    <!-- Include any additional information, assumptions, or context that
    reviewers might need to understand this PR. -->

--- packages/react-native-executorch/common/rnexecutorch/host_objects/JsiConversions.h
+++ packages/react-native-executorch/common/rnexecutorch/host_objects/JsiConversions.h
@@ -29,8 +29,6 @@ template <typename T> T getValue(const jsi::Value &val, jsi::Runtime &runtime);
 template <typename T>
   requires meta::IsNumeric<T>
 inline T getValue(const jsi::Value &val, jsi::Runtime &runtime) {
-  static_assert(std::is_integral<T>::value || std::is_floating_point<T>::value,
-                "Only integral and floating-point types are supported");
   return static_cast<T>(val.asNumber());
 }
 
@@ -54,21 +52,6 @@ getValue<std::shared_ptr<jsi::Function>>(const jsi::Value &val,
 }
 
 template <>
-inline std::vector<int32_t>
-getValue<std::vector<int32_t>>(const jsi::Value &val, jsi::Runtime &runtime) {
-  jsi::Array array = val.asObject(runtime).asArray(runtime);
-  size_t length = array.size(runtime);
-  std::vector<int32_t> result;
-  result.reserve(length);
-
-  for (size_t i = 0; i < length; ++i) {
-    jsi::Value element = array.getValueAtIndex(runtime, i);
-    result.push_back(getValue<int32_t>(element, runtime));
-  }
-  return result;
-}
-
-template <>
 inline JSTensorViewIn getValue<JSTensorViewIn>(const jsi::Value &val,
                                                jsi::Runtime &runtime) {
   jsi::Object obj = val.asObject(runtime);
@@ -83,8 +66,8 @@ inline JSTensorViewIn getValue<JSTensorViewIn>(const jsi::Value &val,
   tensorView.sizes.reserve(numShapeDims);
 
   for (size_t i = 0; i < numShapeDims; ++i) {
-    int dim = getValue<int>(shapeArray.getValueAtIndex(runtime, i), runtime);
-    tensorView.sizes.push_back(static_cast<int32_t>(dim));
+    int32_t dim = getValue<int32_t>(shapeArray.getValueAtIndex(runtime, i), runtime);
+    tensorView.sizes.push_back(dim);
   }
 
   // On JS side, TensorPtr objects hold a 'data' property which should be either
@@ -121,38 +104,6 @@ inline JSTensorViewIn getValue<JSTensorViewIn>(const jsi::Value &val,
         static_cast<uint8_t *>(arrayBuffer.data(runtime)) + byteOffset;
   }
   return tensorView;
-}
-
-template <>
-inline std::vector<JSTensorViewIn>
-getValue<std::vector<JSTensorViewIn>>(const jsi::Value &val,
-                                      jsi::Runtime &runtime) {
-  jsi::Array array = val.asObject(runtime).asArray(runtime);
-  size_t length = array.size(runtime);
-  std::vector<JSTensorViewIn> result;
-  result.reserve(length);
-
-  for (size_t i = 0; i < length; ++i) {
-    jsi::Value element = array.getValueAtIndex(runtime, i);
-    result.push_back(getValue<JSTensorViewIn>(element, runtime));
-  }
-  return result;
-}
-
-template <>
-inline std::vector<std::string>
-getValue<std::vector<std::string>>(const jsi::Value &val,
-                                   jsi::Runtime &runtime) {
-  jsi::Array array = val.asObject(runtime).asArray(runtime);
-  size_t length = array.size(runtime);
-  std::vector<std::string> result;
-  result.reserve(length);
-
-  for (size_t i = 0; i < length; ++i) {
-    jsi::Value element = array.getValueAtIndex(runtime, i);
-    result.push_back(getValue<std::string>(element, runtime));
-  }
-  return result;
 }
 
 // C++ set from JS array. Set with heterogenerous look-up (adding std::less<>
@@ -222,7 +173,26 @@ inline std::vector<T> getArrayAsVector(const jsi::Value &val,
   return result;
 }
 
+
 // Template specializations for std::vector<T> types
+template <>
+inline std::vector<JSTensorViewIn> getValue<std::vector<JSTensorViewIn>>(const jsi::Value &val,
+                                                       jsi::Runtime &runtime) {
+  return getArrayAsVector<JSTensorViewIn>(val, runtime);
+}
+
+template <>
+inline std::vector<std::string> getValue<std::vector<std::string>>(const jsi::Value &val,
+                                                       jsi::Runtime &runtime) {
+  return getArrayAsVector<std::string>(val, runtime);
+}
+
+template <>
+inline std::vector<int32_t> getValue<std::vector<int32_t>>(const jsi::Value &val,
+                                                       jsi::Runtime &runtime) {
+  return getArrayAsVector<int32_t>(val, runtime);
+}
+
 template <>
 inline std::vector<float> getValue<std::vector<float>>(const jsi::Value &val,
                                                        jsi::Runtime &runtime) {
@@ -307,17 +277,17 @@ inline jsi::Value getJsiValue(const std::vector<int32_t> &vec,
   for (size_t i = 0; i < vec.size(); i++) {
     array.setValueAtIndex(runtime, i, jsi::Value(static_cast<int>(vec[i])));
   }
-  return jsi::Value(runtime, array);
+  return {runtime, array};
 }
 
 inline jsi::Value getJsiValue(int val, jsi::Runtime &runtime) {
-  return jsi::Value(runtime, val);
+  return {runtime, val};
 }
 
 inline jsi::Value getJsiValue(const std::shared_ptr<OwningArrayBuffer> &buf,
                               jsi::Runtime &runtime) {
   jsi::ArrayBuffer arrayBuffer(runtime, buf);
-  return jsi::Value(runtime, arrayBuffer);
+  return {runtime, arrayBuffer};
 }
 
 inline jsi::Value
@@ -328,7 +298,7 @@ getJsiValue(const std::vector<std::shared_ptr<OwningArrayBuffer>> &vec,
     jsi::ArrayBuffer arrayBuffer(runtime, vec[i]);
     array.setValueAtIndex(runtime, i, jsi::Value(runtime, arrayBuffer));
   }
-  return jsi::Value(runtime, array);
+  return {runtime, array};
 }
 
 inline jsi::Value getJsiValue(const std::vector<JSTensorViewOut> &vec,
@@ -347,7 +317,7 @@ inline jsi::Value getJsiValue(const std::vector<JSTensorViewOut> &vec,
 
     array.setValueAtIndex(runtime, i, tensorObj);
   }
-  return jsi::Value(runtime, array);
+  return {runtime, array};
 }
 
 inline jsi::Value getJsiValue(const std::string &str, jsi::Runtime &runtime) {
