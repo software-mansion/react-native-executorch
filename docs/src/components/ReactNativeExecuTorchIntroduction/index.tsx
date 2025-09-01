db9c6049b2675501bd5b1fc7a6f5722a11a77e5a
Mateusz Kopcinski <120639731+mkopcins@users.noreply.github.com>
2025-09-01 14:05:47 +0200

    docs: implement changes suggested by marketing team (#577)
    ## Description
    
    Implement various changes, mainly to the landing page to improve seo and
    added carousel with our AI blogs.
    
    ### Introduces a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ### Type of change
    
    - [ ] Bug fix (change which fixes an issue)
    - [ ] New feature (change which adds functionality)
    - [x] Documentation update (improves or adds clarity to existing
    documentation)
    - [ ] Other (chores, tests, code style improvements etc.)
    
    ### Tested on
    
    - [ ] iOS
    - [ ] Android
    
    ### Testing instructions
    
    <!-- Provide step-by-step instructions on how to test your changes.
    Include setup details if necessary. -->
    
    ### Screenshots
    
    <!-- Add screenshots here, if applicable -->
    
    ### Related issues
    
    <!-- Link related issues here using #issue-number -->
    
    ### Checklist
    
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have updated the documentation accordingly
    - [ ] My changes generate no new warnings
    
    ### Additional notes
    
    <!-- Include any additional information, assumptions, or context that
    reviewers might need to understand this PR. -->
    
    ---------
    
    Co-authored-by: Mateusz Kopciński <mateusz.kopcinski@swmansnion.com>

---
+++ docs/src/components/ReactNativeExecuTorchIntroduction/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const ReactNativeExecuTorchIntroduction = () => {
+  return (
+    <div className={styles.introductionContainer}>
+      <h2 className={styles.title}>What is React Native ExecuTorch?</h2>
+      <p className={styles.introduction}>
+        React Native ExecuTorch brings Meta’s ExecuTorch AI framework into the
+        React Native ecosystem, enabling developers to run AI models and LLMs
+        locally, directly on mobile devices. It provides a declarative API for
+        on-device inference, allowing you to use local AI models without relying
+        on cloud infrastructure. Built on the ExecuTorch foundation – part of
+        the PyTorch Edge ecosystem – it extends efficient on-device AI
+        deployment to cross-platform mobile applications in React Native.
+      </p>
+    </div>
+  );
+};
+
+export default ReactNativeExecuTorchIntroduction;
---
+++ docs/src/components/ReactNativeExecutorchAction/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styles from './styles.module.css';
+import AppStore from '@site/static/img/download_app_store.svg';
+import PlayStore from '@site/static/img/download_play_store.svg';
+import PrivateMind from '@site/static/img/private-mind.svg';
+
+const ReactNativeExecutorchAction = () => {
+  return (
+    <div className={styles.featuresContainer}>
+      <h2 className={styles.title}>
+        Want to see our React Native LLMs in action?
+      </h2>
+      <div className={styles.innerContainer}>
+        <div className={styles.introduction}>
+          <p>
+            Download Private Mind – our on-device AI chatbot that works entirely
+            offline.
+          </p>
+          With Private Mind you can:
+          <div className={styles.introduction}>
+            <li className={styles.listItem}>
+              Chat freely with no restrictions.
+            </li>
+            <li className={styles.listItem}>
+              Keep your data safe and private.
+            </li>
+            <li className={styles.listItem}>
+              Browse, test, and benchmark local language models.
+            </li>
+            <li className={styles.listItem}>
+              Customize AI assistants to match your workflow and style.
+            </li>
+          </div>
+        </div>
+        <div className={styles.imageOuterContainer}>
+          <div className={styles.imageInnerContainer}>
+            <PrivateMind height="100%" width="100%" />
+          </div>
+          <div style={{ display: 'flex', width: '100%' }}>
+            <div className={styles.downloadInnerContainer}>
+              <div className={styles.appStore}>
+                <a href="https://apps.apple.com/app/private-mind/id6746713439">
+                  <AppStore height={'80px'} width={''} />
+                </a>
+              </div>
+              <div className={styles.playStore}>
+                <a href="https://play.google.com/store/apps/details?id=com.swmansion.privatemind">
+                  <PlayStore height={'120px'} width={''} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReactNativeExecutorchAction;
