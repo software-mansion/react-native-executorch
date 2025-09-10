23d2fcfdf98d929f2e33da404a1910737095cceb
Anna Szymańska <szymanska.mariaanna@gmail.com>
2025-09-10 11:47:26 +0200

    Update packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Scheduler.cpp
    Co-authored-by: Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>

--- packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Scheduler.cpp
+++ packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Scheduler.cpp
@@ -75,7 +75,7 @@ std::vector<float> Scheduler::step(const std::vector<float> &sample,
                                    const std::vector<float> &noise,
                                    int32_t timestep) {
   if (!numInferenceSteps) {
-    throw "Number of inference steps is not set. Call `set_timesteps` first.";
+    throw std::runtime_error("Number of inference steps is not set. Call `set_timesteps` first.");
   }
 
   size_t noiseSize = noise.size();
