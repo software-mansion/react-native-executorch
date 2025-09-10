2bad64cf6b8e82a5e6d2bb0f97fa6173490c3cfd
Anna Szymańska <szymanska.mariaanna@gmail.com>
2025-09-10 11:48:33 +0200

    Update packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Decoder.h
    Co-authored-by: Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>

--- packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Decoder.h
+++ packages/react-native-executorch/common/rnexecutorch/models/text_to_image/Decoder.h
@@ -6,7 +6,7 @@
 namespace rnexecutorch {
 namespace models::text_to_image {
 
-class Decoder : public BaseModel {
+class Decoder final : public BaseModel {
 public:
   explicit Decoder(const std::string &modelSource, int32_t modelImageSize,
           int32_t numChannels, std::shared_ptr<react::CallInvoker> callInvoker);
