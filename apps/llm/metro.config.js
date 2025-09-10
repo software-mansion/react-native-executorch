c422e403d38480cc758b08ad33056ffc97d9fb36
Jakub Mroz <115979017+jakmro@users.noreply.github.com>
2025-09-10 14:20:44 +0200

    feat: use react-native-audio-api in llm voice chat (#592)
    ## Description
    
    Changes:
    - use react-native-audio-api in llm voice chat example app
    - add audio recording permission
    
    ### Introduces a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ### Type of change
    
    - [x] Bug fix (change which fixes an issue)
    - [ ] New feature (change which adds functionality)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    - [ ] Other (chores, tests, code style improvements etc.)
    
    ### Tested on
    
    - [x] iOS
    - [x] Android
    
    ### Checklist
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have updated the documentation accordingly
    - [x] My changes generate no new warnings

--- apps/llm/metro.config.js
+++ apps/llm/metro.config.js
@@ -1,21 +1,22 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const {
+  wrapWithAudioAPIMetroConfig,
+} = require('react-native-audio-api/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+const { transformer, resolver } = config;
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  };
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
 
-  config.resolver.assetExts.push('pte');
+config.resolver.assetExts.push('pte');
 
-  return config;
-})();
+module.exports = wrapWithAudioAPIMetroConfig(config);
