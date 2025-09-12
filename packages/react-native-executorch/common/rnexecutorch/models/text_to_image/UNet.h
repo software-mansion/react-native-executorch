905daeacf401266ad5b6ef2180b6a38053e4eb5b
Anna Szymańska <szymanska.mariaanna@gmail.com>
2025-09-12 13:11:09 +0200

    Manage input inside UNet

--- packages/react-native-executorch/common/rnexecutorch/models/text_to_image/UNet.h
+++ packages/react-native-executorch/common/rnexecutorch/models/text_to_image/UNet.h
@@ -18,11 +18,11 @@ public:
                 int32_t numChannels,
                 std::shared_ptr<react::CallInvoker> callInvoker);
   std::vector<float> generate(std::vector<float> &latents, int32_t timestep,
-                              std::vector<float> &embeddings);
+                              std::vector<float> &embeddings) const;
 
 private:
-  int32_t modelImageSize;
   int32_t numChannels;
+  int32_t latentsSize;
 };
 } // namespace models::text_to_image
 
