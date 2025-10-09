4c704a811ed1e6d4dff948aba1987cf50909b410
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2025-10-09 18:07:36 +0200

    remove redundant comments from FileUtilsTest.cpp

--- packages/react-native-executorch/common/rnexecutorch/tests/FileUtilsTest.cpp
+++ packages/react-native-executorch/common/rnexecutorch/tests/FileUtilsTest.cpp
@@ -4,31 +4,26 @@
 #include <iostream>
 
 namespace rnexecutorch::file_utils {
-// Test Fixture
 class FileIOTest : public ::testing::Test {
 protected:
   std::string tempFileName = "temp_test_file.txt";
 
   void SetUp() override {
-    // Create a file with some content
     std::ofstream out(tempFileName, std::ios::binary);
     out << "Hello, world";
     out.close();
   }
 
   void TearDown() override {
-    // Delete the file
     std::remove(tempFileName.c_str());
   }
 };
 
-// Positive Test Case
 TEST_F(FileIOTest, LoadBytesFromFileSuccessfully) {
   std::string data = loadBytesFromFile(tempFileName);
   EXPECT_EQ(data, "Hello, world");
 }
 
-// Negative Test Case
 TEST_F(FileIOTest, LoadBytesFromFileFailOnNonExistentFile) {
   EXPECT_THROW(
       { loadBytesFromFile("non_existent_file.txt"); }, std::runtime_error);
