147cc5ae80e481da577ebad0ea47177d144aad22
chmjkb <jakubchmura1607@gmail.com>
2025-12-04 08:57:32 +0100

    chore: update model urls for 0.6 release

--- packages/react-native-executorch/src/constants/modelUrls.ts
+++ packages/react-native-executorch/src/constants/modelUrls.ts
@@ -2,8 +2,8 @@ import { Platform } from 'react-native';
 
 const URL_PREFIX =
   'https://huggingface.co/software-mansion/react-native-executorch';
-const VERSION_TAG = 'resolve/v0.5.0';
-const NEXT_VERSION_TAG = 'resolve/v0.6.0';
+const VERSION_TAG = 'resolve/v0.6.0';
+const NEXT_VERSION_TAG = 'resolve/v0.7.0';
 
 // LLMs
 
