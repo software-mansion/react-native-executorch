40c2e998d0061e7d4a3ba4feb529e8a8c4a6b78f
chmjkb <jakubchmura1607@gmail.com>
2026-01-12 16:43:02 +0100

    feat: update llm runner to use new errros

--- packages/react-native-executorch/common/rnexecutorch/models/llm/LLM.cpp
+++ packages/react-native-executorch/common/rnexecutorch/models/llm/LLM.cpp
@@ -2,6 +2,7 @@
 
 #include <executorch/extension/tensor/tensor.h>
 #include <filesystem>
+#include <rnexecutorch/Error.h>
 #include <rnexecutorch/threads/GlobalThreadPool.h>
 
 namespace rnexecutorch::models::llm {
@@ -19,8 +20,7 @@ LLM::LLM(const std::string &modelSource, const std::string &tokenizerSource,
           std::make_unique<example::Runner>(module_.get(), tokenizerSource)) {
   auto loadResult = runner->load();
   if (loadResult != Error::Ok) {
-    throw std::runtime_error("Failed to load LLM runner, error code: " +
-                             std::to_string(static_cast<int>(loadResult)));
+    throw RnExecutorchError(loadResult, "Failed to load LLM runner");
   }
 
   memorySizeLowerBound = fs::file_size(fs::path(modelSource)) +
@@ -30,7 +30,8 @@ LLM::LLM(const std::string &modelSource, const std::string &tokenizerSource,
 // TODO: add a way to manipulate the generation config with params
 void LLM::generate(std::string input, std::shared_ptr<jsi::Function> callback) {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Runner is not loaded");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Runner is not loaded");
   }
 
   // Create a native callback that will invoke the JS callback on the JS thread
@@ -43,14 +44,14 @@ void LLM::generate(std::string input, std::shared_ptr<jsi::Function> callback) {
   auto config = llm::GenerationConfig{.echo = false, .warming = false};
   auto error = runner->generate(input, config, nativeCallback, {});
   if (error != executorch::runtime::Error::Ok) {
-    throw std::runtime_error("Failed to generate text, error code: " +
-                             std::to_string(static_cast<int>(error)));
+    throw RnExecutorchError(error, "Failed to generate text");
   }
 }
 
 void LLM::interrupt() {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Can't interrupt a model that's not loaded!");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Can't interrupt a model that's not loaded");
   }
   runner->stop();
 }
@@ -68,28 +69,32 @@ size_t LLM::getMemoryLowerBound() const noexcept {
 
 void LLM::setCountInterval(size_t countInterval) {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Can't configure a model that's not loaded!");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Can't configure a model that's not loaded");
   }
   runner->set_count_interval(countInterval);
 }
 
 void LLM::setTimeInterval(size_t timeInterval) {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Can't configure a model that's not loaded!");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Can't configure a model that's not loaded");
   }
   runner->set_time_interval(timeInterval);
 }
 
 void LLM::setTemperature(float temperature) {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Can't configure a model that's not loaded!");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Can't configure a model that's not loaded");
   }
   runner->set_temperature(temperature);
 };
 
 void LLM::setTopp(float topp) {
   if (!runner || !runner->is_loaded()) {
-    throw std::runtime_error("Can't configure a model that's not loaded!");
+    throw RnExecutorchError(RnExecutorchInternalError::ModuleNotLoaded,
+                            "Can't configure a model that's not loaded");
   }
   runner->set_topp(topp);
 }
