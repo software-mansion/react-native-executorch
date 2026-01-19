eb9902b5a23adf8fa3ae6538a77fc65d87e1d630
Mateusz Sluszniak <56299341+msluszniak@users.noreply.github.com>
2026-01-19 11:05:03 +0100

    Update packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.cpp

--- packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.cpp
+++ packages/react-native-executorch/common/rnexecutorch/data_processing/dsp.cpp
@@ -10,7 +10,8 @@ namespace rnexecutorch::dsp {
 
 using std::numbers::pi_v;
 
-std::vector<float> hannWindow(size_t size) { 
+//https://www.mathworks.com/help/signal/ref/hann.html
+std::vector<float> hannWindow(size_t size) {
   std::vector<float> window(size); 
   for (size_t i = 0; i < size; i++) { 
     window[i] = 0.5f * (1.0f - std::cosf(2.0f * pi_v<float> * static_cast<float>(i) / static_cast<float>(size)));
