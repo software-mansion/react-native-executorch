c5e34fcdbbb90279e375a16ebf14e5e34a3fce1a
mateuszlampert <mateuszlampert@gmail.com>
2026-01-26 19:47:50 +0100

    feat: unify dropdown styling

---
+++ docs/src/theme/CopyPageContent/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CopyPageContent from '@theme-original/CopyPageContent';
+import './styles.module.css';
+
+export default function CopyPageContentWrapper(props) {
+  return (
+    <>
+      <CopyPageContent {...props} />
+    </>
+  );
+}
