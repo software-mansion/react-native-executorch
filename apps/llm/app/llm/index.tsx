d2eed3cd810b1ff776ab6725cfdf7096681aa37d
mateuszlampert <mateuszlampert@gmail.com>
2026-02-19 03:09:20 +0100

    chore: update llms example app with strategy configuration

--- apps/llm/app/llm/index.tsx
+++ apps/llm/app/llm/index.tsx
@@ -11,7 +11,11 @@ import {
   View,
 } from 'react-native';
 import SendIcon from '../../assets/icons/send_icon.svg';
-import { useLLM, LLAMA3_2_1B_SPINQUANT } from 'react-native-executorch';
+import {
+  useLLM,
+  LLAMA3_2_1B_SPINQUANT,
+  SlidingWindowContextStrategy,
+} from 'react-native-executorch';
 import PauseIcon from '../../assets/icons/pause_icon.svg';
 import ColorPalette from '../../colors';
 import Messages from '../../components/Messages';
@@ -31,6 +35,15 @@ function LLMScreen() {
   const { setGlobalGenerating } = useContext(GeneratingContext);
 
   const llm = useLLM({ model: LLAMA3_2_1B_SPINQUANT });
+
+  useEffect(() => {
+    llm.configure({
+      chatConfig: {
+        contextStrategy: new SlidingWindowContextStrategy(2048, 512),
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (llm.error) {
