8101a44f32b9358bc8b0fcadd9987a4316cf6790
Jakub Chmura <92989966+chmjkb@users.noreply.github.com>
2026-02-20 15:25:17 +0100

    Update packages/react-native-executorch/src/constants/commonVision.ts
    Co-authored-by: Mateusz Kopcinski <120639731+mkopcins@users.noreply.github.com>

--- packages/react-native-executorch/src/constants/commonVision.ts
+++ packages/react-native-executorch/src/constants/commonVision.ts
@@ -1,4 +1,4 @@
 import { Triple } from '../types/common';
 
-export const IMAGENET_MEAN: Triple<number> = [0.485, 0.456, 0.406];
-export const IMAGENET_STD: Triple<number> = [0.229, 0.224, 0.225];
+export const IMAGENET1K_MEAN: Triple<number> = [0.485, 0.456, 0.406];
+export const IMAGENET1K_STD: Triple<number> = [0.229, 0.224, 0.225];
