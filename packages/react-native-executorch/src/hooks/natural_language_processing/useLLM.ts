7133ca496777d4a35c243a7ad3a01a946b926f5b
mateuszlampert <mateuszlampert@gmail.com>
2026-02-23 17:45:43 +0100

    test: ci should fail

--- packages/react-native-executorch/src/hooks/natural_language_processing/useLLM.ts
+++ packages/react-native-executorch/src/hooks/natural_language_processing/useLLM.ts
@@ -14,7 +14,7 @@ import { RnExecutorchError, parseUnknownError } from '../../errors/errorUtils';
  *
  * @category Hooks
  * @param model - Object containing model, tokenizer, and tokenizer config sources.
- * @returns An object implementing the `LLMType` interface for interacting with the LLM.
+ * @returns An object implementing the `LLMType` interface for interacting with the LLM. asdasdasd
  */
 export const useLLM = ({ model, preventLoad = false }: LLMProps): LLMType => {
   const [token, setToken] = useState<string>('');
