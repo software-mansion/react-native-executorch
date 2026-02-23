84fd1781485c5146627cfa25109a43d980bb58d6
mateuszlampert <mateuszlampert@gmail.com>
2026-02-23 15:59:30 +0100

    chore: add prettier step to build scripts

--- docs/scripts/build-version-docs.sh
+++ docs/scripts/build-version-docs.sh
@@ -7,7 +7,7 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
-yarn docusaurus docs:version $VERSION
+yarn docusaurus docs:version $VERSION && npx prettier --write .
 
 find versioned_docs/version-$VERSION -type f \( -name "*.md" -o -name "*.mdx" \) \
   -exec sed -i "" "s|/blob/main/|/blob/$SHA/|g" {} +
