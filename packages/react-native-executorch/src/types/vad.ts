bc334df5f0b5f2c041ab523c4186c5b2d3fefaf3
IgorSwat <igorswat2002@o2.pl>
2026-05-10 09:50:23 +0200

    Implement VAD streaming

--- packages/react-native-executorch/src/types/vad.ts
+++ packages/react-native-executorch/src/types/vad.ts
@@ -32,6 +32,30 @@ export interface Segment {
 }
 
 /**
+ * Configuration options for the VAD streaming process.
+ * @category Types
+ * @property {number} [timeout] - Specifies (in milliseconds) how much does streamer wait between model inferences.
+ * @property {number} [detectionMargin] - Specifies (in milliseconds) how far the last detected speech segment can be to still be considered as ongoing speech.
+ */
+export interface VADStreamingOptions {
+  timeout?: number;
+  detectionMargin?: number;
+}
+
+/**
+ * Input configuration for the VAD streaming hook.
+ * @category Types
+ * @property {() => void | Promise<void>} [onSpeechBegin] - Callback function triggered when speech is detected.
+ * @property {() => void | Promise<void>} [onSpeechEnd] - Callback function triggered when speech end (silence is detected).
+ * @property {VADStreamingOptions} [options] - Optional configuration for the streaming process.
+ */
+export interface VADStreamingInput {
+  onSpeechBegin?: () => void | Promise<void>;
+  onSpeechEnd?: () => void | Promise<void>;
+  options?: VADStreamingOptions;
+}
+
+/**
  * React hook state and methods for managing a Voice Activity Detection (VAD) model instance.
  * @category Types
  */
@@ -63,4 +87,22 @@ export interface VADType {
    * @throws {RnExecutorchError} If the model is not loaded or is currently processing another request.
    */
   forward(waveform: Float32Array): Promise<Segment[]>;
+
+  /**
+   * Starts a streaming Voice Activity Detection session.
+   * @param input - Configuration for streaming, including callbacks for speech begin/end and optional parameters.
+   * @returns A promise that resolves when the streaming session stops.
+   */
+  stream(input: VADStreamingInput): Promise<void>;
+
+  /**
+   * Inserts an audio chunk into the streaming VAD session.
+   * @param waveform - The audio data to add to the buffer.
+   */
+  streamInsert(waveform: Float32Array): void;
+
+  /**
+   * Stops the current streaming VAD session.
+   */
+  streamStop(): void;
 }
