ff22f175092521463003427f6320cdd69c1648fd
Mateusz Kopcinski <120639731+mkopcins@users.noreply.github.com>
2026-07-28 11:47:03 +0200

    feat: added system and bundleId to telemetry to version 0.8 (#1312)
    ## Description
    
    Added two new fields to model downloads telemetry: system and bundleId.
    
    ### Introduces a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ### Type of change
    
    - [ ] Bug fix (change which fixes an issue)
    - [x] New feature (change which adds functionality)
    - [ ] Documentation update (improves or adds clarity to existing
    documentation)
    - [ ] Other (chores, tests, code style improvements etc.)
    
    ### Tested on
    
    - [x] iOS
    - [x] Android
    
    ### Testing instructions
    
    Run any new model (to trigger a download event) and check if new event
    with that bundle id is registered on the website
    https://ai.swmansion.com/telemetry/admin/downloads
    
    ### Checklist
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have updated the documentation accordingly
    - [x] My changes generate no new warnings
    
    ### Additional notes
    
    <!-- Include any additional information, assumptions, or context that
    reviewers might need to understand this PR. -->
    
    ---------
    
    Co-authored-by: Mateusz Słuszniak <mateusz.sluszniak@swmansion.com>

--- packages/react-native-executorch/android/src/main/cpp/ETInstallerModule.h
+++ packages/react-native-executorch/android/src/main/cpp/ETInstallerModule.h
@@ -24,7 +24,7 @@ public:
 
   static void registerNatives();
 
-  void injectJSIBindings();
+  void injectJSIBindings(jni::alias_ref<jstring> bundleId);
 
 private:
   friend HybridBase;
--- packages/react-native-executorch/src/utils/ResourceFetcherUtils.ts
+++ packages/react-native-executorch/src/utils/ResourceFetcherUtils.ts
@@ -1,10 +1,7 @@
+import { Platform } from 'react-native';
 import { ResourceSource } from '..';
 import { getModelNameForUrl } from '../constants/modelUrls';
-import {
-  DOWNLOAD_EVENT_ENDPOINT,
-  LIB_VERSION,
-} from '../constants/resourceFetcher';
-
+import { DOWNLOAD_EVENT_ENDPOINT } from '../constants/resourceFetcher';
 /**
  * Http status codes
  * @category Types
@@ -213,6 +210,10 @@ export namespace ResourceFetcherUtils {
     return global.__rne_isEmulator;
   }
 
+  export function getBundleId(): string {
+    return global.__rne_bundleId;
+  }
+
   function getModelNameFromUri(uri: string): string {
     const knownName = getModelNameForUrl(uri);
     if (knownName) {
@@ -228,18 +229,18 @@ export namespace ResourceFetcherUtils {
    * @param uri - The URI of the downloaded resource.
    */
   export function triggerDownloadEvent(uri: string) {
-    try {
-      fetch(DOWNLOAD_EVENT_ENDPOINT, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          modelName: getModelNameFromUri(uri),
-          countryCode: getCountryCode(),
-          isEmulator: isEmulator(),
-          libVersion: LIB_VERSION,
-        }),
-      });
-    } catch (e) {}
+    fetch(DOWNLOAD_EVENT_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        bundleId: getBundleId(),
+        countryCode: getCountryCode(),
+        isEmulator: isEmulator(),
+        libVersion: require('../../package.json').version,
+        modelName: getModelNameFromUri(uri),
+        system: Platform.OS,
+      }),
+    }).catch((_) => {});
   }
 
   /**
